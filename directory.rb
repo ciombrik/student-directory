7a5ce959767b948ac5621d13b76058d84a39db76
Alex <alexandrachorbe@MA324.local>
2021-05-31 14:20:35 +0100

    completed task 7 (Asking for user input)

--- directory.rb
+++ directory.rb
@@ -1,13 +1,31 @@
-puts "The students of Villains Academmy"
-puts "--------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Normam Bates"
+
+def print_header
+  puts "The students of the villains Academy"
+  puts "------------"
+end 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit the return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do 
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp 
+  end 
+  return students 
+end 
+
+def print(students)
+  students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+def print_footer(students)
+  puts "Overall, we have #{students.count} students"
+end 
+
+ students = input_students 
+ print_header
+ print(students)
+ print_footer(students) 
